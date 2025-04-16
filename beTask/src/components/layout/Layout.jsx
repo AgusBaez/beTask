b8c82b02ab2e2d0a5a7ae3d3a00ff0ec1d4f5d19
Agus Baez <agustinbaezignacio@gmail.com>
2025-04-15 22:04:30 -0300

    📦 New Component Layout

---
+++ beTask/src/components/layout/Layout.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar.jsx";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Navbar />
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
+
--- beTask/src/main.jsx
+++ beTask/src/main.jsx
@@ -2,14 +2,17 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
 import TodoList from "./components/TodoList";
+import Layout from "./components/layout/Layout";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<TodoList />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<TodoList />} />
+      </Routes>
+    </Layout>
   </BrowserRouter>
 );
 
